cf7cb1cd0eff0e5b0a8da00aa754e05611766846
Nodirbek Usmanov <nodirbek.usmanov@dkbcodefactory.com>
2020-09-30 22:09:12 +0200

    valdiate a json with the json schema in version draft-07

---
+++ src/main/java/com/example/demojsonschemavalidation07/DemoJsonSchemaValidation07Application.java
@@ -0,0 +1,13 @@
+package com.example.demojsonschemavalidation07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJsonSchemaValidation07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJsonSchemaValidation07Application.class, args);
+	}
+
+}
